88990fb30077234e8484f40c4bcab7e5f6cb188d
Brandon Lim <enryu100@hotmail.com>
2018-11-03 14:29:08 +0800

    working doxy, plus last few days of updates
    something screwed up, was forced to re-download repo, and re-commit everything

--- Source Code/godotVS/modules/aiemotion/aiemotion.cpp
+++ Source Code/godotVS/modules/aiemotion/aiemotion.cpp
@@ -7,9 +7,9 @@ void AIEmotion::normalizeEmotions(){
 	// check if total means there could be more than 1 value over threshold
   double total;
 	total	= fabs(joy_sadness) + fabs(trust_disgust) + fabs(fear_anger) + fabs(surprise_anticipation);
-	if(total > 19.7d){
+	if(total > (double)19.7){
 		//normalization needed
-		double ratio = 19.7d / total;
+		double ratio = (double)19.7 / total;
 		joy_sadness *= ratio;
 		trust_disgust *= ratio;
 		fear_anger *= ratio;
@@ -21,9 +21,9 @@ int AIEmotion::addJoy(double joyVal){
 	joy_sadness += (joyVal * m_joy);
 	normalizeEmotions();
 	if(joy_sadness > emotion_threshold){
-		return (1);
+		return (int)1;
 	} else {
-		return (0);
+		return (int)0;
 	}
 }
 
@@ -31,9 +31,9 @@ int AIEmotion::addSad(double sadVal){
 	joy_sadness -= (sadVal * m_sadness);
 	normalizeEmotions();
 	if(joy_sadness < (emotion_threshold*-1)){
-		return (1);
+		return (int)1;
 	} else {
-		return (0);
+		return (int)0;
 	}
 }
 
@@ -41,9 +41,9 @@ int AIEmotion::addTrust(double trustVal){
 	trust_disgust += (trustVal * m_trust);
 	normalizeEmotions();
 	if(trust_disgust > emotion_threshold){
-		return (1);
+		return (int)1;
 	} else {
-		return (0);
+		return (int)0;
 	}
 }
 
@@ -51,9 +51,9 @@ int AIEmotion::addDisgust(double disgVal){
 	trust_disgust -= (disgVal * m_disgust);
 	normalizeEmotions();
 	if(trust_disgust < (emotion_threshold*-1)){
-		return (1);
+		return (int)1;
 	} else {
-		return (0);
+		return (int)0;
 	}
 }
 
@@ -61,9 +61,9 @@ int AIEmotion::addFear(double fearVal){
 	fear_anger += (fearVal * m_fear);
 	normalizeEmotions();
 	if(fear_anger > emotion_threshold){
-		return (1);
+		return (int)1;
 	} else {
-		return (0);
+		return (int)0;
 	}
 }
 
@@ -71,9 +71,9 @@ int AIEmotion::addAnger(double angVal){
 	fear_anger -= (angVal * m_anger);
 	normalizeEmotions();
 	if(fear_anger < (emotion_threshold*-1)){
-		return (1);
+		return (int)1;
 	} else {
-		return (0);
+		return (int)0;
 	}
 }
 
@@ -81,9 +81,9 @@ int AIEmotion::addSurprise(double surpVal){
 	surprise_anticipation += (surpVal * m_surprise);
 	normalizeEmotions();
 	if(surprise_anticipation > emotion_threshold){
-		return (1);
+		return (int)1;
 	} else {
-		return (0);
+		return (int)0;
 	}
 }
 
@@ -91,18 +91,57 @@ int AIEmotion::addAnticipation(double anticVal){
 	surprise_anticipation -= (anticVal * m_anticipation);
 	normalizeEmotions();
 	if(surprise_anticipation < (emotion_threshold*-1)){
-		return (1);
+		return (int)1;
 	} else {
-		return (0);
+		return (int)0;
+	}
+}
+
+void AIEmotion::calcViewData(){
+	int state = 0;
+	
+	//calculate emotional state based on emotion values
+	if (joy_sadness > 0){
+		state++;
+	} else {
+		if(joy_sadness < 0){
+			state--;
+		}
+	}
+	if (trust_disgust > 0){
+		state++;
+	} else {
+		if(trust_disgust < 0){
+			state--;
+		}
+	}
+	if (fear_anger > 0){
+		state++;
+	} else {
+		if(fear_anger < 0){
+			state--;
+		}
+	}
+	if (surprise_anticipation > 0){
+		state++;
+	} else {
+		if(surprise_anticipation < 0){
+			state--;
+		}
+	}
+	
+	//translate state to view angle and depth
+	if(state > 0){
+		aov = 120.0;
+		dov = 50.0;
+	} else if (state < 0){
+		aov = 90.0;
+		dov = 100.0;
 	}
 }
 
 void AIEmotion::setEmotionThreshold(double e_threshhold){
 	emotion_threshold = e_threshhold;
-}
-
-void AIEmotion::setDecayRate(r_decay){
-	decay_rate = r_decay;
 }
 
 void AIEmotion::decayEmotions(){
@@ -136,68 +175,73 @@ void AIEmotion::setEmotionLevels(double joy, double trust, double fear, double s
 	surprise_anticipation = surprise;
 }
 
-void AIEmotion::setAgentModifiers(double joy_m, double sadness_m, double trust_m, double disgust_m, double fear_m, double anger_m, double surprise_m, double anticipation_m){
+void AIEmotion::setJoyModifier(double joy_m){
 	m_joy = joy_m;
-	m_sadness = sadness_m;
+}
+void AIEmotion::setTrustModifier(double trust_m){
 	m_trust = trust_m;
-	m_disgust = disgust_m;
+}
+void AIEmotion::setFearModifier(double fear_m){
 	m_fear = fear_m;
-	m_anger = anger_m;
+}
+void AIEmotion::setSurpriseModifier(double surprise_m){
 	m_surprise = surprise_m;
+}
+void AIEmotion::setSadnessModifier(double sadness_m){
+	m_sadness = sadness_m;
+}
+void AIEmotion::setDisgustModifier(double disgust_m){
+	m_disgust = disgust_m;
+}
+void AIEmotion::setAngerModifier(double anger_m){
+	m_anger = anger_m;
+}
+void AIEmotion::setAnticipationModifier(double anticipation_m){
 	m_anticipation = anticipation_m;
 }
 
 AIEmotion::AIEmotion(){
-	joy_sadness = 0.0d;
-	trust_disgust = 0.0d;
-	fear_anger = 0.0d;
-	surprise_anticipation = 0.0d;
-	m_joy = 1.0d;
-	m_sadness = 1.0d;
-	m_trust = 1.0d;
-	m_disgust = 1.0d;
-	m_fear = 1.0d;
-	m_anger = 1.0d;
-	m_surprise = 1.0d;
-	m_anticipation = 1.0d;
-	emotion_threshold = 5.0d;
-	decay_rate = 0.05d;
+	joy_sadness = 0.0;
+	trust_disgust = 0.0;
+	fear_anger = 0.0;
+	surprise_anticipation = 0.0;
+	m_joy = 1.0;
+	m_sadness = 1.0;
+	m_trust = 1.0;
+	m_disgust = 1.0;
+	m_fear = 1.0;
+	m_anger = 1.0;
+	m_surprise = 1.0;
+	m_anticipation = 1.0;
+	emotion_threshold = 5.0;
+	decay_rate = 0.05;
+	dov = 100.0;
+	aov = 120.0;
 }
 
-AIEmotion::AIEmotion(double joy, double trust, double fear, double surprise, double joy_m, double trust_m, double fear_m, double surprise_m, double sadness_m, double disgust_m, double anger_m, double anticipation_m){
-	joy_sadness = joy;
-	trust_disgust = trust;
-	fear_anger = fear;
-	surprise_anticipation = surprise;
-	m_joy = joy_m;
-	m_sadness = sadness_m;
-	m_trust = trust_m;
-	m_disgust = disgust_m;
-	m_fear = fear_m;
-	m_anger = anger_m;
-	m_surprise = surprise_m;
-	m_anticipation = anticipation_m;
-	emotion_threshold = 5.0d;
-	decay_rate = 0.05d;
-}
-
-static void AIEmotion::_bind_methods(){
-	ClassDB::register_method(D_METHOD("addJoy", "joyVal"), &addJoy);
-	ClassDB::register_method(D_METHOD("addSad", "sadVal"), &addSad);
-	ClassDB::register_method(D_METHOD("addTrust", "trustVal"), &addTrust);
-	ClassDB::register_method(D_METHOD("addDisgust", "disgVal"), &addDisgust);
-	ClassDB::register_method(D_METHOD("addFear", "fearVal"), &addFear);
-	ClassDB::register_method(D_METHOD("addAnger", "angVal"), &addAnger);
-	ClassDB::register_method(D_METHOD("addSurprise", "surpVal"), &addSurprise);
-	ClassDB::register_method(D_METHOD("addAnticipation", "anticVal"), &addAnticipation);
-	ClassDB::register_method(D_METHOD("setEmotionLevels", "joy", "trust", "fear", "surprise"), &setEmotionLevels);
-	ClassDB::register_method(D_METHOD("setAgentModifiers","joy_m", "sadness_m", "trust_m", "disgust_m", "fear_m", "anger_m", "surprise_m", "anticipation_m"), &setAgentModifiers);
-	ClassDB::register_method(D_METHOD("AIEmotion","joy", "trust", "fear", "surprise", "joy_m", "trust_m", "fear_m", "surprise_m", "sadness_m", "disgust_m", "anger_m", "anticipation_m"), &AIEmotion);
-	ClassDB::register_method(D_METHOD("AIEmotion"), &AIEmotion);
-	ADD_PROPERTY(PropertyInfo(Variant::double, "joy_sadness"), "setNothing","getJoySad");
-	ADD_PROPERTY(PropertyInfo(Variant::double, "trust_disgust"), "setNothing","getTrustDisgust");
-	ADD_PROPERTY(PropertyInfo(Variant::double, "fear_anger"), "setNothing","getFearAnger");
-	ADD_PROPERTY(PropertyInfo(Variant::double, "surprise_anticipation"), "setNothing","getSurpriseAnticipation");
+void AIEmotion::_bind_methods(){
+	ClassDB::bind_method("addJoy", &AIEmotion::addJoy);
+	ClassDB::bind_method("addSad", &AIEmotion::addSad);
+	ClassDB::bind_method("addTrust", &AIEmotion::addTrust);
+	ClassDB::bind_method("addDisgust", &AIEmotion::addDisgust);
+	ClassDB::bind_method("addFear", &AIEmotion::addFear);
+	ClassDB::bind_method("addAnger", &AIEmotion::addAnger);
+	ClassDB::bind_method("addSurprise", &AIEmotion::addSurprise);
+	ClassDB::bind_method("addAnticipation", &AIEmotion::addAnticipation);
+	ClassDB::bind_method("setEmotionLevels", &AIEmotion::setEmotionLevels);
+	ClassDB::bind_method("setAngerModifier", &AIEmotion::setAngerModifier);
+	ClassDB::bind_method("setJoyModifier", &AIEmotion::setJoyModifier);
+	ClassDB::bind_method("setFearModifier", &AIEmotion::setFearModifier);
+	ClassDB::bind_method("setSadnessModifier", &AIEmotion::setSadnessModifier);
+	ClassDB::bind_method("setTrustModifier", &AIEmotion::setTrustModifier);
+	ClassDB::bind_method("setDisgustModifier", &AIEmotion::setDisgustModifier);
+	ClassDB::bind_method("setSurpriseModifier", &AIEmotion::setSurpriseModifier);
+	ClassDB::bind_method("setAnticipationModifier", &AIEmotion::setAnticipationModifier);
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "joy_sadness"), "setNothing","getJoySad");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "trust_disgust"), "setNothing","getTrustDisgust");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "fear_anger"), "setNothing","getFearAnger");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "surprise_anticipation"), "setNothing","getSurpriseAnticipation");
+	ADD_PROPERTY(PropertyInfo(Variant::REAL, "e_threshhold"), "setEmotionThreshold", "getEmotionThreshold");
 }
 
 void AIEmotion::setNothing(){
