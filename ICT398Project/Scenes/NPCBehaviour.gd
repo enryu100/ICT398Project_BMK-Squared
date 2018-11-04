d42f61a08d5803ced24c801aa897b207162079e4
Kennif <kennif.work@gmail.com>
2018-11-04 21:39:47 +0800

    Put code into NPC class

--- ICT398Project/Scenes/NPCBehaviour.gd
+++ ICT398Project/Scenes/NPCBehaviour.gd
@@ -1,24 +1,25 @@
 extends MeshInstance
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-var ai = AIEmotion.new()
-
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	setBaseline()
-	pass
-
-func _process(delta):
-	# Called every frame. Delta is time since last frame.
-	# Update game logic here.
-	var current_pos = Man.get_global_pos
-	pass
-
-func setBaseline():
-	ai.setEmotionLevels(4.0, 2.0, 1.0, 0.0)
+class NPC:
+	# class member variables go here, for example:
+	# var a = 2
+	# var b = "textvar"
+	var ai = AIEmotion.new()
+	var position
 	
-	pass
+	func _ready():
+		# Called when the node is added to the scene for the first time.
+		# Initialization here
+		setBaseline()
+		pass
+	
+	func _process(delta):
+		# Called every frame. Delta is time since last frame.
+		# Update game logic here.
+		var current_pos = Man.get_global_pos
+		pass
+	
+	func setBaseline():
+		ai.setEmotionLevels(4.0, 2.0, 1.0, 0.0)
+		
+		pass
